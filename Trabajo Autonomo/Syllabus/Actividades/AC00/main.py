73b3099e6273790e4b8beac5f01f84ad843c7194
Carlos <cegallegos@uc.cl>
2020-12-26 11:19:17 -0300

    Actividad 00 resuelta

--- Trabajo Autonomo/Syllabus/Actividades/AC00/main.py
+++ Trabajo Autonomo/Syllabus/Actividades/AC00/main.py
@@ -4,13 +4,24 @@ from collections import namedtuple, defaultdict
 # Para esta parte necesitarás los contenidos de la semana 0
 def cargar_datos(path):
     # Para esta función te puede servir el cuaderno 3 de la semana 0
-    return None
-        
+    with open('ayudantes.csv','r') as data:
+        ayudantes = []
+        for row in data:
+            ayudante = row.strip()
+            ayudantes.append(ayudante)
+    return ayudantes
 
 # De aquí en adelante necesitarás los contenidos de la semana 1
 def crear_ayudantes(datos):
     # Completar función
-    return None
+    ayudante = namedtuple('Ayudante', ['Nombre', 'Cargo', 'Usuario'])
+    ayudantes = []
+    for row in datos:
+        ayudante_list = row.split(',')
+        ayudantes.append(ayudante(ayudante_list[0],
+                                  ayudante_list[1],
+                                  ayudante_list[2])) 
+    return ayudantes[1::]
 
 def encontrar_cargos(ayudantes):
     # Completar función
@@ -18,7 +29,24 @@ def encontrar_cargos(ayudantes):
 
 def ayudantes_por_cargo(ayudantes):
     # Completar función
-    return None
+    cargos = ['Full Docencia', 'Full Tareas', 'HÃ\xadbrido Docencia', 'HÃ\xadbrido Tareas']
+    full_docencia = []
+    full_tareas = []
+    hibrido_docencia = []
+    hibrido_tareas = []
+    for row in ayudantes:
+        ayudante= row.split(',')
+        if ayudante[1] == 'Full Docencia':
+            full_docencia.append(ayudante[0])
+        elif ayudante[1] == 'Full_Tareas':
+            full_tareas.append(ayudante[0])
+        elif ayudante[1] == 'HÃ\xadbrido Docencia':
+            hibrido_docencia.append(ayudante[0])
+        else:
+            hibrido_tareas.append(ayudante[0])
+    ayudantes_por_cargo = [full_docencia, full_tareas, hibrido_docencia, hibrido_tareas]
+    ayudante_dic = dict(zip(cargos, ayudantes_por_cargo))
+    return ayudante_dic
 
 
 if __name__ == '__main__':
